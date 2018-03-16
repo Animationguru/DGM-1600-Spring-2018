77eebf229ad77e7bd1d940206a26a2c6c56cfbb8
Cody Kimball <cody_kmbll@yahoo.com>
2018-03-15 19:42:46 -0600

    It All Works Now

--- DGM Chicken Game/Assets/Scripts/wander.cs
+++ DGM Chicken Game/Assets/Scripts/wander.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class wander : MonoBehaviour {
+	public float moveSpeed;
+	public Transform target;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	//public int damage;
+
+	//public GameObject pcHealth;
 	
-	// Update is called once per frame
-	void Update () {
+	public void Wandering(){
+	
+	//makes the animal wander
+	transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+	int randomnum = Random.Range(0,360);
+	Vector3 fwd = transform.TransformDirection(Vector3.forward);
+	RaycastHit hit;
+
+
+	Debug.DrawRay(transform.position,fwd*3, Color.red);
+
+		if(Physics.Raycast(transform.position,fwd,out hit,3)){
+		
+			if(hit.collider.tag == "wall"){
+			transform.Rotate(0,randomnum,0);
+			}
+		}
+	}	
+	void OnTriggerStay(Collider other){
+		if(other.gameObject.tag == "Player"){
+			if(target == null){target = other.gameObject.GetComponent<Transform>();}
+			Follow();
+		}
+	}
+	void Follow(){
+		transform.LookAt(target);
+		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 		
 	}
 }
